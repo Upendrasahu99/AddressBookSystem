237b76a9e118ca8178aa1264ca6817f49bc095b1
upendra <upendrasahu1199@gmail.com>
2023-02-06 22:51:25 +0530

    Ability to add a new Contact to Address Book

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,23 +1,41 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class AddressBook {
+    static Scanner sc = new Scanner(System.in);
+    static List<Contacts>contactsList;
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
-        Contacts contacts = new Contacts("Anil", "Kumar", "9996663331", "anilk@gmail.com", "Delhi", "New Delhi", "100001");
-        System.out.println("First Name : " + contacts.first_name);
-        System.out.println("Last Name : " + contacts.last_name);
-        System.out.println("Phone Number : " + contacts.phone_number);
-        System.out.println("email ID : " + contacts.email);
-        System.out.println("City : " + contacts.city);
-        System.out.println("State : " + contacts.state);
-        System.out.println("Zipcode : " + contacts.zipcode);
+        contactsList = new ArrayList<>();
+        System.out.println("Add new Contact");
+        System.out.print("Enter First Name : ");
+        String firstName = sc.next();
+        System.out.print("Enter Last Name : ");
+        String lastName = sc.next();
+        System.out.print("Enter Phone : ");
+        String phoneNumber = sc.next();
+        System.out.print("Enter Email : ");
+        String email = sc.next();
+        System.out.print("Enter City : ");
+        String city = sc.next();
+        System.out.print("Enter State : ");
+        String state = sc.next();
+        System.out.print("Enter Zip : ");
+        String zipcode = sc.next();
+
+        Contacts contacts = new Contacts(firstName,lastName,phoneNumber,email,city,state,zipcode);
+        contactsList.add(contacts);
+        System.out.println("Contact Added to the Address Book");
+        System.out.println(contacts);
     }
+
 }
-
 class Contacts {
-    String first_name, last_name, city, state, email, phone_number, zipcode;
-
-    public Contacts(String first_name, String last_name, String phone_number, String email, String city, String state, String zipcode) {
+    String first_name,last_name,city,state,email,phone_number,zipcode;
+    public Contacts(String first_name,String last_name,String phone_number,String email,String city,String state,String zipcode) {
         this.first_name = first_name;
         this.last_name = last_name;
         this.phone_number = phone_number;
@@ -25,5 +43,9 @@ class Contacts {
         this.city = city;
         this.state = state;
         this.zipcode = zipcode;
+    }
+    @Override
+    public String toString() {
+        return "First Name : '" + first_name + "', Last Name : '" + last_name + "', Phone Number : '" + phone_number + "', email ID : '" + email + "', City : '" + city + "', State : '" + state + "', Zipcode : '" + zipcode + "'";
     }
 }
